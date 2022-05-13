f498bd490496fb06ad4d74bb049433fb988806d4
shoi.ito <shoi.ito@mixi.co.jp>
2022-05-13 18:49:03 +0900

    update decorator pattern

--- Decorator.cs
+++ Decorator.cs
@@ -16,6 +16,11 @@ namespace Decorator
     {
         private Component component;
 
+        public Decorator(Component component)
+        {
+            this.component = component;
+        }
+
         public override void Operation()
         {
             component.Operation();
@@ -26,6 +31,10 @@ namespace Decorator
     {
         private int someParam;
 
+        public ConcreteDecoratorA(Component component) : base(component)
+        {
+        }
+
         public override void Operation()
         {
             base.Operation();
@@ -34,6 +43,10 @@ namespace Decorator
 
     public class ConcreteDecoratorB : Decorator
     {
+        public ConcreteDecoratorB(Component component) : base(component)
+        {
+        }
+
         public override void Operation()
         {
             base.Operation();
@@ -42,6 +55,18 @@ namespace Decorator
 
         public void SomeFunction()
         {
+        }
+    }
+
+    public class SomeApplication
+    {
+        public void SomeFunction()
+        {
+            Component comp = new ConcreteComponent();
+            Component deco1 = new ConcreteDecoratorA(comp);
+            Component deco2 = new ConcreteDecoratorB(comp);
+            Component deco3 = new ConcreteDecoratorA(new ConcreteDecoratorB(comp));
+            Component deco4 = new ConcreteDecoratorA(new ConcreteDecoratorB(new ConcreteDecoratorB(comp)));
         }
     }
 }
